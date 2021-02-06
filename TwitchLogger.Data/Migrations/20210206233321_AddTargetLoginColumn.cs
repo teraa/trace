f0cc4fd78dd7bd207636f423982f59b372bb47ee
Tera <24725862+teraa@users.noreply.github.com>
2021-02-07 00:35:33 +0100

    add migration

---
+++ TwitchLogger.Data/Migrations/20210206233321_AddTargetLoginColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwitchLogger.Data.Migrations
+{
+    public partial class AddTargetLoginColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "target_login",
+                schema: "twitch",
+                table: "moderator_action",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "target_login",
+                schema: "twitch",
+                table: "moderator_action");
+        }
+    }
+}
