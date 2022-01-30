975bd0c15eeab85d43ab691cd45e2b03b962dc02
Tera <24725862+teraa@users.noreply.github.com>
2022-01-30 21:50:42 +0100

    top level statements

--- TwitchLogger/Program.cs
+++ TwitchLogger/Program.cs
@@ -1,47 +1,36 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Twitch.Irc;
 using Twitch.PubSub;
+using TwitchLogger;
 using TwitchLogger.Data;
 using TwitchLogger.Options;
 using TwitchLogger.Services;
 
-namespace TwitchLogger;
-
-public class Program
-{
-    public static async Task Main(string[] args)
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseSystemd()
+    .ConfigureServices((hostContext, services) =>
     {
-        var host = CreateHostBuilder(args).Build();
+        services
+            .AddDbContextFactory<TwitchLoggerDbContext>(options =>
+                options.UseNpgsql(hostContext.Configuration["DB_STRING"]))
 
-        var contextFactory = host.Services.GetRequiredService<IDbContextFactory<TwitchLoggerDbContext>>();
-        using (var ctx = contextFactory.CreateDbContext())
-        {
-            await ctx.Database.MigrateAsync();
-        }
+            .AddOptionsWithSection<ChatOptions>(hostContext.Configuration)
+            .AddTwitchIrcClient()
+            .AddHostedService<ChatService>()
 
-        await host.RunAsync();
-    }
+            .AddOptionsWithSection<PubSubOptions>(hostContext.Configuration)
+            .AddTwitchPubSubClient()
+            .AddHostedService<PubSubService>()
+            ;
+    })
+    .Build();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseSystemd()
-            .ConfigureServices((hostContext, services) =>
-            {
-                services
-                    .AddDbContextFactory<TwitchLoggerDbContext>(options =>
-                        options.UseNpgsql(Environment.GetEnvironmentVariable("DB_STRING")!))
-
-                    .AddOptionsWithSection<ChatOptions>(hostContext.Configuration)
-                    .AddTwitchIrcClient()
-                    .AddHostedService<ChatService>()
-
-                    .AddOptionsWithSection<PubSubOptions>(hostContext.Configuration)
-                    .AddTwitchPubSubClient()
-                    .AddHostedService<PubSubService>()
-                    ;
-            });
+var contextFactory = host.Services.GetRequiredService<IDbContextFactory<TwitchLoggerDbContext>>();
+using (var ctx = contextFactory.CreateDbContext())
+{
+    await ctx.Database.MigrateAsync();
 }
+
+await host.RunAsync();
