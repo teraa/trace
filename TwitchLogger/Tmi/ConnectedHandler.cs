11f57bfbc22b5cb0ebd559f4982794d63a0de1e9
Tera <24725862+teraa@users.noreply.github.com>
2022-04-20 20:01:11 +0200

    split out handlers to separate files

---
+++ TwitchLogger/Tmi/ConnectedHandler.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Teraa.Irc;
+using Teraa.Twitch.Tmi;
+using Teraa.Twitch.Tmi.Notifications;
+using TwitchLogger.Data;
+
+namespace TwitchLogger.Tmi;
+
+public class ConnectedHandler : INotificationHandler<Connected>
+{
+    private readonly TmiService _tmi;
+    private readonly TwitchLoggerDbContext _ctx;
+    private readonly ILogger<ConnectedHandler> _logger;
+
+    public ConnectedHandler(
+        TmiService tmi,
+        TwitchLoggerDbContext ctx,
+        ILogger<ConnectedHandler> logger)
+    {
+        _tmi = tmi;
+        _ctx = ctx;
+        _logger = logger;
+    }
+
+    public async Task Handle(Connected notification, CancellationToken cancellationToken)
+    {
+        _tmi.EnqueueMessage(new Message
+        {
+            Command = Command.NICK,
+            Content = new("justinfan1"),
+        });
+
+        _tmi.EnqueueMessage(new Message
+        {
+            Command = Command.CAP,
+            Arg = "REQ",
+            Content = new("twitch.tv/tags twitch.tv/commands")
+        });
+
+        var channels = await _ctx.ChatLogs
+            .Select(x => x.Channel.Login)
+            .ToListAsync(cancellationToken);
+
+        _logger.LogInformation("Joining: {Channels}", channels);
+
+        foreach (string channel in channels)
+            _tmi.EnqueueMessage(new Message
+            {
+                Command = Command.JOIN,
+                Content = new($"#{channel}"),
+            });
+    }
+}
--- TwitchLogger/Tmi/MessageHandler.cs
+++ TwitchLogger/Tmi/MessageHandler.cs
@@ -1,59 +1,11 @@
 using System.Diagnostics;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Teraa.Irc;
-using Teraa.Twitch.Tmi;
 using Teraa.Twitch.Tmi.Notifications;
 using TwitchLogger.Data;
 
-namespace TwitchLogger.Services;
-
-public class ConnectedHandler : INotificationHandler<Connected>
-{
-    private readonly TmiService _tmi;
-    private readonly TwitchLoggerDbContext _ctx;
-    private readonly ILogger<ConnectedHandler> _logger;
-
-    public ConnectedHandler(
-        TmiService tmi,
-        TwitchLoggerDbContext ctx,
-        ILogger<ConnectedHandler> logger)
-    {
-        _tmi = tmi;
-        _ctx = ctx;
-        _logger = logger;
-    }
-
-    public async Task Handle(Connected notification, CancellationToken cancellationToken)
-    {
-        _tmi.EnqueueMessage(new Message
-        {
-            Command = Command.NICK,
-            Content = new("justinfan1"),
-        });
-
-        _tmi.EnqueueMessage(new Message
-        {
-            Command = Command.CAP,
-            Arg = "REQ",
-            Content = new("twitch.tv/tags twitch.tv/commands")
-        });
-
-        var channels = await _ctx.ChatLogs
-            .Select(x => x.Channel.Login)
-            .ToListAsync(cancellationToken);
-
-        _logger.LogInformation("Joining: {Channels}", channels);
-
-        foreach (string channel in channels)
-            _tmi.EnqueueMessage(new Message
-            {
-                Command = Command.JOIN,
-                Content = new($"#{channel}"),
-            });
-    }
-}
+namespace TwitchLogger.Tmi;
 
 public class MessageHandler : INotificationHandler<MessageReceived>
 {
