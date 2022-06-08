2eca128c3e51aebb1d877b22f3d9a507a939f3db
Tera <24725862+teraa@users.noreply.github.com>
2022-06-08 23:58:03 +0200

    update scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dotnet restore
-dotnet publish -c Release -r linux-x64 -o bin --self-contained false /p:PublishSingleFile=true /p:DebugType=None /p:UseSharedCompilation=false /nodeReuse:false TwitchLogger/
+echo "Building"
+dotnet publish -c Release -r linux-x64 -o bin/publish/ --self-contained false /p:PublishSingleFile=true /p:DebugType=None /p:UseSharedCompilation=false /nodeReuse:false TwitchLogger/
