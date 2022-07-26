f853710e6e2f91b9e66620a4f28bbe20801024b3
Tera <24725862+teraa@users.noreply.github.com>
2022-07-26 12:04:45 +0200

    add messages endpoint

---
+++ TwitchLogger.Api/Features/Messages/Actions/Index.cs
@@ -0,0 +1,47 @@
+﻿using JetBrains.Annotations;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TwitchLogger.Data;
+
+namespace TwitchLogger.Api.Features.Messages.Actions;
+
+public static class Index
+{
+    public record Query(string ChannelId) : IRequest<IActionResult>;
+
+    [PublicAPI]
+    public record Result(
+        long Id,
+        DateTimeOffset Timestamp,
+        string AuthorId,
+        string AuthorLogin,
+        string Content);
+
+    [UsedImplicitly]
+    public class Handler : IRequestHandler<Query, IActionResult>
+    {
+        private readonly TwitchLoggerDbContext _ctx;
+
+        public Handler(TwitchLoggerDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<IActionResult> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var results = await _ctx.TmiMessages
+                .Where(x => x.ChannelId == request.ChannelId)
+                .OrderByDescending(x => x.Timestamp)
+                .Select(x => new Result(
+                    x.Id,
+                    x.Timestamp,
+                    x.AuthorId,
+                    x.AuthorLogin,
+                    x.Content))
+                .ToListAsync(cancellationToken);
+
+            return new OkObjectResult(results);
+        }
+    }
+}
---
+++ TwitchLogger.Api/Features/Messages/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Index = TwitchLogger.Api.Features.Messages.Actions.Index;
+
+namespace TwitchLogger.Api.Features.Messages;
+
+[ApiController]
+[Route("[controller]")]
+public class MessagesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public MessagesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index(string channelId, CancellationToken cancellationToken)
+        => await _mediator.Send(new Index.Query(channelId), cancellationToken);
+}
