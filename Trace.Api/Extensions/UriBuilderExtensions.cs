d0c18a1f497033357cb768bb28ce8534f25348ca
Tera <24725862+teraa@users.noreply.github.com>
2022-08-03 00:16:42 +0200

    add UriBuilderExtensions.cs

---
+++ Trace.Api/Extensions/UriBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Trace.Api.Extensions;
+
+public static class UriBuilderExtensions
+{
+    public static UriBuilder AddQuery(this UriBuilder uriBuilder, IReadOnlyDictionary<string, string?> query)
+    {
+        var pairs = query.Select(x => x.Value is null
+            ? $"{Uri.EscapeDataString(x.Key)}"
+            : $"{Uri.EscapeDataString(x.Key)}={Uri.EscapeDataString(x.Value)}");
+
+        uriBuilder.Query = string.Join('&', pairs);
+
+        return uriBuilder;
+    }
+}
