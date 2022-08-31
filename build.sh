cba6aad3bc8f26bc0ffb4f887df048c63840515c
Tera <24725862+teraa@users.noreply.github.com>
2022-08-31 22:40:02 +0200

    reorder opts

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Building"
-dotnet publish -c Release -r linux-x64 -o bin/publish/ --self-contained false -p:PublishSingleFile=true -p:DebugType=None -p:UseSharedCompilation=false -nodeReuse:false Trace/
+dotnet publish -c Release -r linux-x64 --self-contained false -o bin/publish/ -p:PublishSingleFile=true -p:DebugType=None -p:UseSharedCompilation=false -nodeReuse:false Trace/
