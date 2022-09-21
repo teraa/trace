566bdec2b7effd95b37a05eec9146c34bd2b9c46
Tera <24725862+teraa@users.noreply.github.com>
2022-09-21 21:30:42 +0200

    dont generate docs on publish

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 echo "Building"
 dotnet publish Trace \
   -c Release -r linux-x64 --self-contained false -o bin/publish/ \
+  -p:GenerateDocumentationFile=false \
   -p:PublishSingleFile=true \
   -p:DebugType=None \
   -p:UseSharedCompilation=false \
