1d08ad602e6dd7d7af16b85597e7e6e490ea7b0e
Tera <24725862+teraa@users.noreply.github.com>
2022-09-21 21:14:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "Building"
-dotnet publish -c Release -r linux-x64 --self-contained false -o bin/publish/ -p:PublishSingleFile=true -p:DebugType=None -p:UseSharedCompilation=false -nodeReuse:false Trace/
+dotnet publish Trace \
+  -c Release -r linux-x64 --self-contained false -o bin/publish/ \
+  -p:PublishSingleFile=true \
+  -p:DebugType=None \
+  -p:UseSharedCompilation=false \
+  -nodeReuse:false
