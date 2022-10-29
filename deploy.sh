ea5d92f9ccd4e54c27abadfb4b0b3ec6c3db132d
Tera <24725862+teraa@users.noreply.github.com>
2022-10-29 22:52:19 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
-﻿#!/bin/bash
+#!/bin/bash
 # # edit sudoers file to allow for passwordless service stop/start
 # $ sudo visudo -f /etc/sudoers.d/trace
 # %sudo ALL=NOPASSWD: /bin/systemctl stop trace.service, /bin/systemctl start trace.service
-
 svc=trace.service
 
+echo -n "Status: "
 systemctl is-active $svc
 is_active=$?
+
+# exit on first error
+set -e
 
 if [[ $is_active -eq 0 ]]; then
   echo "Stopping service"
@@ -14,7 +17,7 @@ if [[ $is_active -eq 0 ]]; then
 fi
 
 echo "Moving files"
-rm -r bin/current
+rm -rf bin/current
 mv bin/{publish,current}
 
 if [[ $is_active -eq 0 ]]; then
