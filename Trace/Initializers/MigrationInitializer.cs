be55676920c148f61e626d36a64c7687a902bae2
Tera <24725862+teraa@users.noreply.github.com>
2022-11-08 17:02:28 +0100

    revert MigrationInitializer.cs

--- Trace/Initializers/MigrationInitializer.cs
+++ Trace/Initializers/MigrationInitializer.cs
@@ -2,7 +2,6 @@
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using Trace.Data;
-using Trace.Data.Models.Twitch;
 
 namespace Trace.Initializers;
 
@@ -19,55 +18,5 @@ public class MigrationInitializer : IAsyncInitializer
     public async Task InitializeAsync()
     {
         await _ctx.Database.MigrateAsync();
-
-        var since = await _ctx.TwitchUsers.MaxAsync(x => x.LastSeen);
-
-        var users = await _ctx.TmiMessages
-            .Where(x => x.Timestamp > since)
-            .GroupBy(x => x.AuthorId)
-            .Select(x => new
-            {
-                Id = x.Key,
-                Entries = x.Select(static x => new
-                    {
-                        x.AuthorLogin,
-                        x.Timestamp,
-                    })
-                    .OrderBy(static x => x.Timestamp)
-                    .ToList()
-            })
-            .ToListAsync();
-
-        var entries = new List<User>();
-
-        foreach (var user in users)
-        {
-            User? entry = await _ctx.TwitchUsers
-                .Where(x => x.Id == user.Id)
-                .OrderByDescending(x => x.LastSeen)
-                .FirstOrDefaultAsync();
-
-            foreach (var userEntry in user.Entries)
-            {
-                if (entry?.Login == userEntry.AuthorLogin)
-                {
-                    entry.LastSeen = userEntry.Timestamp;
-                }
-                else
-                {
-                    entry = new User
-                    {
-                        Id = user.Id,
-                        Login = userEntry.AuthorLogin,
-                        FirstSeen = userEntry.Timestamp,
-                        LastSeen = userEntry.Timestamp,
-                    };
-                    entries.Add(entry);
-                }
-            }
-        }
-
-        _ctx.TwitchUsers.AddRange(entries);
-        await _ctx.SaveChangesAsync();
     }
 }
