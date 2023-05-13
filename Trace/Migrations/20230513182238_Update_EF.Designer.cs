1ff6c34ecccc626ccd0026c976088cc5f209c5b7
Tera <24725862+teraa@users.noreply.github.com>
2023-05-13 20:23:44 +0200

    create migration for EF 7

--- Trace/Migrations/20230513182238_Update_EF.Designer.cs
+++ Trace/Migrations/20230513182238_Update_EF.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Trace.Data;
@@ -11,9 +12,11 @@ using Trace.Data;
 namespace Trace.Migrations
 {
     [DbContext(typeof(TraceDbContext))]
-    partial class TraceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230513182238_Update_EF")]
+    partial class Update_EF
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
