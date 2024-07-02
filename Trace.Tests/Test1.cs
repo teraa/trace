81f384ffbeef5566283ebede3e3e08847f12bda5
Tera <24725862+teraa@users.noreply.github.com>
2024-07-02 20:55:46 +0200

    add example test

---
+++ Trace.Tests/Test1.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Trace.Data;
+using Trace.Data.Models.Twitch;
+
+namespace Trace.Tests;
+
+public class Test1 : IClassFixture<AppFactory>
+{
+    private readonly AppFactory _appFactory;
+
+    public Test1(AppFactory appFactory)
+    {
+        _appFactory = appFactory;
+    }
+
+    [Fact]
+    public async Task TestAsync()
+    {
+        using var scope = _appFactory.Services.CreateScope();
+        var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        ctx.TwitchUsers.Add(new User
+        {
+            EntryId = 0,
+            Login = "example",
+        });
+
+        await ctx.SaveChangesAsync();
+
+        var count = await ctx.TwitchUsers.CountAsync();
+        count.Should().Be(1);
+
+        await _appFactory.ResetDatabaseAsync();
+
+        count = await ctx.TwitchUsers.CountAsync();
+        count.Should().Be(0);
+    }
+}
