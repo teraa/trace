8652f5e200448536a8fc97e2a4bba4099fe127e0
Tera <24725862+teraa@users.noreply.github.com>
2024-07-07 15:57:08 +0200

    add xml doc

--- Trace/Api/AppClaimTypes.cs
+++ Trace/Api/AppClaimTypes.cs
@@ -2,7 +2,16 @@ namespace Trace.Api;
 
 public static class AppClaimTypes
 {
+    /// <summary>
+    /// Authenticated user's Twitch account ID.
+    /// </summary>
     public const string TwitchId = "twitch.id";
+    /// <summary>
+    /// Authenticated user's Twitch account login.
+    /// </summary>
     public const string TwitchLogin = "twitch.login";
+    /// <summary>
+    /// Permission to read channel's data.
+    /// </summary>
     public const string ChannelRead = "channel.read";
 }
