454830a549ba2c398a6299b48274cebff4e7cd41
Tera <24725862+teraa@users.noreply.github.com>
2024-07-09 19:56:05 +0200

    refactor Message.IndexTests

--- Trace.Tests/Twitch/Messages/IndexTests.cs
+++ Trace.Tests/Twitch/Messages/IndexTests.cs
@@ -13,6 +13,8 @@ namespace Trace.Tests.Twitch.Messages;
 [Collection("1")]
 public sealed class IndexTests : IAsyncLifetime, IDisposable
 {
+    private const string s_channelId = "channel.id";
+
     private readonly AppFactory _appFactory;
     private readonly IServiceScope _scope;
     private readonly Index.Handler _handler;
@@ -26,7 +28,22 @@ public sealed class IndexTests : IAsyncLifetime, IDisposable
         _scope = _appFactory.Services.CreateScope();
         _handler = _scope.ServiceProvider.GetRequiredService<Index.Handler>();
         _ctx = _scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        _appFactory.SetUser([new Claim(AppClaimTypes.ChannelRead, ChannelId)]);
     }
+
+    private static string ChannelId => s_channelId;
+
+    private Message Template { get; } = new()
+    {
+        AuthorId = "author.id",
+        AuthorLogin = "author.login",
+        ChannelId = "channel.id",
+        Content = "Lorem ipsum",
+        Source = new Source {Name = "source"},
+        Timestamp = DateTimeOffset.MinValue,
+    };
+
+    private Index.Query Query { get; } = new(s_channelId);
 
     public Task InitializeAsync()
     {
@@ -43,20 +60,10 @@ public sealed class IndexTests : IAsyncLifetime, IDisposable
         _scope.Dispose();
     }
 
-    private void SetChannelRead(string channelId)
-    {
-        _appFactory.SetUser([new Claim(AppClaimTypes.ChannelRead, channelId)]);
-    }
-
-
     [Fact]
     public async Task Forbidden_WhenUnauthorized()
     {
-        SetChannelRead("foo");
-        var request = new Index.Query(
-            ChannelId: "bar",
-            Limit: 1
-        );
+        var request = new Index.Query("foo");
 
         var actionResult = await _handler.HandleAsync(request);
 
@@ -66,36 +73,22 @@ public sealed class IndexTests : IAsyncLifetime, IDisposable
     [Fact]
     public async Task Returns_CorrectChannelMessages()
     {
-        const string channelId = "channel.id";
-        SetChannelRead(channelId);
-        var template = new Message
-        {
-            AuthorId = "author.id",
-            AuthorLogin = "author.login",
-            ChannelId = channelId,
-            Content = "Lorem ipsum",
-            Source = new Source {Name = "source"},
-            Timestamp = DateTimeOffset.MinValue,
-        };
-
         var messages = new List<Message>
         {
-            template with {Id = 1},
-            template with {Id = 2, ChannelId = "foo"},
-            template with {Id = 3, ChannelId = "bar"},
-            template with {Id = 4},
-            template with {Id = 5, ChannelId = "foo"},
-            template with {Id = 6, ChannelId = "bar"},
+            Template with {Id = 1},
+            Template with {Id = 2, ChannelId = "foo"},
+            Template with {Id = 3, ChannelId = "bar"},
+            Template with {Id = 4},
+            Template with {Id = 5, ChannelId = "foo"},
+            Template with {Id = 6, ChannelId = "bar"},
         };
 
         _ctx.TmiMessages.AddRange(messages);
         await _ctx.SaveChangesAsync();
 
-        var query = new Index.Query(channelId);
-
 
         // Act
-        var response = await _handler.HandleAsync(query);
+        var response = await _handler.HandleAsync(Query);
 
 
         response.Should().BeResults()
@@ -106,34 +99,22 @@ public sealed class IndexTests : IAsyncLifetime, IDisposable
     [Fact]
     public async Task QueryWithBefore_RespectsTimestampNotId()
     {
-        const string channelId = "channel.id";
-        SetChannelRead(channelId);
         var start = DateTimeOffset.MinValue;
         var second = new TimeSpan(0, 0, 1);
-        var template = new Message
-        {
-            AuthorId = "author.id",
-            AuthorLogin = "author.login",
-            ChannelId = channelId,
-            Content = "Lorem ipsum",
-            Source = new Source {Name = "source"},
-            Timestamp = start,
-        };
 
         var messages = new List<Message>
         {
-            template with {Id = 1, Timestamp = start + second * 1}, // before
-            template with {Id = 5, Timestamp = start + second * 2}, // before
-            template with {Id = 3, Timestamp = start + second * 3}, // before
-            template with {Id = 2, Timestamp = start + second * 4}, // pivot
-            template with {Id = 4, Timestamp = start + second * 5}, // after
+            Template with {Id = 1, Timestamp = start + second * 1}, // before
+            Template with {Id = 5, Timestamp = start + second * 2}, // before
+            Template with {Id = 3, Timestamp = start + second * 3}, // before
+            Template with {Id = 2, Timestamp = start + second * 4}, // pivot
+            Template with {Id = 4, Timestamp = start + second * 5}, // after
         };
 
         _ctx.TmiMessages.AddRange(messages);
-
         await _ctx.SaveChangesAsync();
 
-        var query = new Index.Query(template.ChannelId, Before: 2);
+        var query = Query with {Before = 2};
 
 
         // Act
@@ -149,34 +130,20 @@ public sealed class IndexTests : IAsyncLifetime, IDisposable
     [Fact]
     public async Task QueryWithInvalidBefore_ReturnsBadRequest()
     {
-        const string channelId = "channel.id";
-        SetChannelRead(channelId);
-        var template = new Message
-        {
-            AuthorId = "author.id",
-            AuthorLogin = "author.login",
-            ChannelId = channelId,
-            Content = "Lorem ipsum",
-            Source = new Source {Name = "source"},
-            Timestamp = DateTimeOffset.MinValue,
-        };
-
         var messages = new List<Message>
         {
-            template with {Id = 1},
-            template with {Id = 2, ChannelId = "other"},
+            Template with {Id = 1},
+            Template with {Id = 2, ChannelId = "foo"},
         };
 
         _ctx.TmiMessages.AddRange(messages);
-
         await _ctx.SaveChangesAsync();
 
-        var query = new Index.Query(template.ChannelId, Before: 2);
+        var query = Query with {Before = 2};
 
 
         // Act
         var response = await _handler.HandleAsync(query);
-
 
         response.Should().BeOfType<BadRequestObjectResult>();
     }
@@ -184,33 +151,20 @@ public sealed class IndexTests : IAsyncLifetime, IDisposable
     [Fact]
     public async Task QueryWithAuthorId_ReturnsOnlyMessagesByAuthorId()
     {
-        const string channelId = "channel.id";
-        SetChannelRead(channelId);
-        var template = new Message
-        {
-            AuthorId = "author.id",
-            AuthorLogin = "author.login",
-            ChannelId = channelId,
-            Content = "Lorem ipsum",
-            Source = new Source {Name = "source"},
-            Timestamp = DateTimeOffset.MinValue,
-        };
-
         var messages = new List<Message>
         {
-            template with {Id = 1, AuthorId = "1"}, // target
-            template with {Id = 2, AuthorId = "2"},
-            template with {Id = 3, AuthorId = "3"},
-            template with {Id = 4, AuthorId = "1"}, // target
-            template with {Id = 5, AuthorId = "5"},
+            Template with {Id = 1, AuthorId = "1"}, // target
+            Template with {Id = 2, AuthorId = "2"},
+            Template with {Id = 3, AuthorId = "3"},
+            Template with {Id = 4, AuthorId = "1"}, // target
+            Template with {Id = 5, AuthorId = "5"},
         };
 
         _ctx.TmiMessages.AddRange(messages);
-
         await _ctx.SaveChangesAsync();
 
         const string authorId = "1";
-        var query = new Index.Query(template.ChannelId, AuthorId: authorId);
+        var query = Query with {AuthorId = authorId};
 
 
         // Act
@@ -229,33 +183,20 @@ public sealed class IndexTests : IAsyncLifetime, IDisposable
     [Fact]
     public async Task QueryWithBeforeTimestamp_ReturnsOnlyMessagesBeforeTimestamp()
     {
-        const string channelId = "channel.id";
-        SetChannelRead(channelId);
-
         var timestamp = DateTimeOffset.MinValue;
         var second = TimeSpan.FromSeconds(1);
 
-        var template = new Message
-        {
-            AuthorId = "author.id",
-            AuthorLogin = "author.login",
-            ChannelId = channelId,
-            Content = "Lorem ipsum",
-            Source = new Source {Name = "source"},
-            Timestamp = timestamp,
-        };
-
         var messages = new List<Message>
         {
-            template with {Id = 1, Timestamp = timestamp + second * 0},
-            template with {Id = 2, Timestamp = timestamp + second * 1},
-            template with {Id = 3, Timestamp = timestamp + second * 2},
+            Template with {Id = 1, Timestamp = timestamp + second * 0},
+            Template with {Id = 2, Timestamp = timestamp + second * 1},
+            Template with {Id = 3, Timestamp = timestamp + second * 2},
         };
 
         _ctx.TmiMessages.AddRange(messages);
         await _ctx.SaveChangesAsync();
 
-        var query = new Index.Query(channelId, BeforeTimestamp: messages[1].Timestamp);
+        var query = Query with {BeforeTimestamp = messages[1].Timestamp};
 
 
         // Act
