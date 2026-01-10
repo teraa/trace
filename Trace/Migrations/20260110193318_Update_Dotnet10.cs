8213072cba492139b83df43384a3336bb6721b70
Tera <24725862+teraa@users.noreply.github.com>
2026-01-10 20:34:58 +0100

    add migration

---
+++ Trace/Migrations/20260110193318_Update_Dotnet10.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trace.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Dotnet10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_configs",
+                schema: "tmi",
+                table: "configs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_configs1",
+                schema: "tmi",
+                table: "configs",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_configs1",
+                schema: "tmi",
+                table: "configs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_configs",
+                schema: "tmi",
+                table: "configs",
+                column: "id");
+        }
+    }
+}
